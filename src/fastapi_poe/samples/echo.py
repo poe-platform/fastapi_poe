dbc9191d48849a7f00edf82e230c5415a40f216a
Kris Yang <145800990+krisyang1125@users.noreply.github.com>
2023-09-23 19:06:14 -0700

    Update query.query to request.query
    Update local variable with black formatting

--- src/fastapi_poe/samples/echo.py
+++ src/fastapi_poe/samples/echo.py
@@ -14,7 +14,9 @@ from fastapi_poe.types import PartialResponse, QueryRequest
 
 
 class EchoBot(PoeBot):
-    async def get_response(self, request: QueryRequest) -> AsyncIterable[PartialResponse]:
+    async def get_response(
+        self, request: QueryRequest
+    ) -> AsyncIterable[PartialResponse]:
         last_message = request.query[-1].content
         yield PartialResponse(text=last_message)
 
