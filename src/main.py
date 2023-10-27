b790b625f8263c18455b81928612eb25b2eb608d
Mark <mark.choi@tangibleidea.net>
2023-10-28 00:40:56 +1100

    host ip change.

--- src/main.py
+++ src/main.py
@@ -47,4 +47,4 @@ async def concat_message(apikey, request, botname):
         concated = concated + partial.text
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+   uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
