6837b8e7076a54e51276edd307b2ad4f644b14db
fhennerkes <fhennerkes@quora.com>
2025-04-08 17:32:33 -0700

    Monetization: Adding monetization related parameters to SettingsResponse

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,6 @@
 import pydantic
 import pytest
-from fastapi_poe.types import PartialResponse
+from fastapi_poe.types import CostItem, PartialResponse
 
 
 def test_extra_attrs() -> None:
@@ -10,3 +10,18 @@ def test_extra_attrs() -> None:
     resp = PartialResponse(text="a capybara", is_replace_response=True)
     assert resp.is_replace_response is True
     assert resp.text == "a capybara"
+
+
+def test_cost_item() -> None:
+    with pytest.raises(pydantic.ValidationError):
+        CostItem(amount_usd_milli_cents=1.5)  # type: ignore
+
+    with pytest.raises(pydantic.ValidationError):
+        CostItem(amount_usd_milli_cents="1")  # type: ignore
+
+    with pytest.raises(pydantic.ValidationError):
+        CostItem(amount_usd_milli_cents=-2.5)  # type: ignore
+
+    item = CostItem(amount_usd_milli_cents=25)
+    assert item.amount_usd_milli_cents == 25
+    assert item.description is None
